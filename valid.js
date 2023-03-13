cffa0b4ece8832ecd206f95560b96cc79450402c
Wendesday36 <bertazoeanna@gmail.com>
2023-03-13 16:43:21 +0100

    name validalasa

--- valid.js
+++ valid.js
@@ -1,6 +1,9 @@
 function validateForm() {
 	let x = document.validTeszt.knev.value;
-	if (x === "") {
+	let y = document.validTeszt.Vnev.value;
+	let z = document.validTeszt.jlsz1.value;
+	let v = document.validTeszt.jlsz2.value;
+	if (x === "", y === " ",z === " ",v === "") {
     	alert("A mező kitöltése kötelező!");
     	return false;
 	}
