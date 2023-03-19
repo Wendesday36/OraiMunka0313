da4f0bb5f475089b5e9882f93f914d17051c1c3f
Wendesday36 <bertazoeanna@gmail.com>
2023-03-19 12:35:42 +0100

    prettier

--- valid.js
+++ valid.js
@@ -1,6 +1,5 @@
 function validateForm() {
   let x = document.validTeszt.knev.value;
-
   if (x === "") {
     alert("A mező kitöltése kötelező!");
     return false;
@@ -22,57 +21,51 @@ function azonos() {
 }
 function jelszobaKotelezo() {
   if (jlsz1.length < 8) {
-    alert(
-      "A jelszo nem lehet kisebb mint 8 karakter")
+    alert("A jelszo nem lehet kisebb mint 8 karakter");
     return false;
   }
   if (jlsz2.length < 8) {
-    alert(
-      "A jelszo nem lehet kisebb mint 8 karakter")
+    alert("A jelszo nem lehet kisebb mint 8 karakter");
     return false;
   }
-  
+
   if (jlsz1 != jlsz2) {
-    alert(
-      "A jelszavak nem egyeznek!!")
+    alert("A jelszavak nem egyeznek!!");
     return false;
   }
   var upperCaseLetters = /[A-Z]/g;
   if (jlsz1.value.match(upperCaseLetters)) {
-	valid = true
+    valid = true;
   } else {
-    alert("nagy betut is kell tartalmaznia")
+    alert("nagy betut is kell tartalmaznia");
   }
   if (jlsz2.value.match(upperCaseLetters)) {
-    valid = true
+    valid = true;
   } else {
-	alert("nagy betut is kell tartalmaznia")
+    alert("nagy betut is kell tartalmaznia");
   }
-
 
   var lowerCaseLetters = /[a-z]/g;
   if (jlsz1.value.match(lowerCaseLetters)) {
-    valid = true
+    valid = true;
   } else {
-    alert("kisbetut is kell tartalmaznia")
+    alert("kisbetut is kell tartalmaznia");
   }
   if (jlsz2.value.match(lowerCaseLetters)) {
-    valid = true
+    valid = true;
   } else {
-   alert("kisbetut is kell tartalmaznia")
+    alert("kisbetut is kell tartalmaznia");
   }
-
 
   var numbers = /[0-9]/g;
-  if(jlsz1.value.match(numbers)){
-    valid =true
+  if (jlsz1.value.match(numbers)) {
+    valid = true;
   } else {
-    alert("Számot is kell tartalmaznia!")
+    alert("Számot is kell tartalmaznia!");
   }
-  if(jlsz2.value.match(numbers)){
-    valid =true
+  if (jlsz2.value.match(numbers)) {
+    valid = true;
   } else {
-    alert("Számot is kell tartalmaznia!")
+    alert("Számot is kell tartalmaznia!");
   }
-  
 }
